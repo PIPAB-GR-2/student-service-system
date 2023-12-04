6ea528c495d733c634668f108588275a5b5a3df1
jacek <jacek.677369@gmail.com>
2023-12-04 19:20:29 +0100

    Lista 6 zad 5

---
+++ SOS/src/main/java/pl/edu/wwsis/pipab/App.java
@@ -0,0 +1,12 @@
+package pl.edu.wwsis.pipab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
